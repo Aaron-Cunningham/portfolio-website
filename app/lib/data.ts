545f9f16fe41792957e61a564f7b5cb8b4c56315
Aaron-Cunningham <aaroncunningham222@gmail.com>
2024-01-21 13:04:57 +0000

    -Add data file

---
+++ app/lib/data.ts
@@ -0,0 +1,105 @@
+import React from "react";
+import { LuGraduationCap } from "react-icons/lu";
+import LotteryWebApp from "../../../../Desktop/portfolio/public/LotteryWebApp.png";
+import translateImg from "../../../../Desktop/portfolio/public/translate.png";
+import AirBnbIMG from "../../../../Desktop/portfolio/public/airbnb.png"
+
+
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    id: 1,
+    title: "Computing Student",
+    location: "Middlesbrough College, UK",
+    description: "Finished my course at college. Here I learned about Java, WebDev, Databases, Life cycle, and Mathematics. I finished the course with 45/45 credits at Distinction.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2021-2022",
+  },
+  {
+    id: 2,
+    title: "Computer Science Student",
+    location: "Newcastle University, UK",
+    description: "Full-time student studying Computer Science. Focusing on Programming, discrete Mathematics, Databases, Web Security, Networks, Operating Systems, and much more!",
+    icon: React.createElement(LuGraduationCap),
+    date: "2022 - 2025",
+  },
+] as const;
+
+
+export const projectsData = [
+  {
+    title: "AirBnb Clone (Frontend)",
+    description:
+        "This app is still a work in progress (last update 05/01/2024). The app will have a modern design, with multiple choices of car rentals.",
+    tags: ["Next.js", "Tailwind", "TypeScript", "JavaScript", "Map-GL", "GeoLib", "React-date-range"],
+    imageUrl: AirBnbIMG,
+    sourceCode: "https://github.com/Aaron-Cunningham/AirBnbClone",
+    liveSite: 'https://air-bnb-topaz.vercel.app/'
+  },
+  {
+    title: "LotteryWebApp",
+    description:
+      "This app allows users to submit numbers for a lottery draw and also allows admins to play draws. This app is very security heavy with lots of authentication, authorisation, logging, and more!",
+    tags: ["Python", "Flask", "MYSQL", "JavaScript", "HTML"],
+    imageUrl: LotteryWebApp,
+    sourceCode: "https://github.com/Aaron-Cunningham/LotteryWebApp",
+    liveSite: false
+  },
+  {
+    title: "TranslatorApp",
+    description:
+      "This is a very basic translator app using Google Translate API",
+    tags: ["React", "JavaScript", "HTML", "CSS", "RESTful API"],
+    imageUrl: translateImg,
+    sourceCode: "https://github.com/Aaron-Cunningham/Translator-webapp",
+    liveSite: "https://translator-webapp.vercel.app/"
+  },
+] as const;
+
+export const skillsData = [
+
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Flask",
+  "Django",
+  "Git",
+  "Tailwind",
+  "PostgreSQL",
+  "Python",
+  "Java",
+  "Hiberate",
+  "IntelliJ",
+  "PyCharm",
+  "VS Code"
+] as const;
